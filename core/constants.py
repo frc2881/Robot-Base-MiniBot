503b098497666d31a4fa18ab399500070b724380
Lady Cans <frc2881@gmail.com>
2025-12-12 19:52:13 -0600

    Update robot-lib, PV beta, navX2 Micro

--- core/constants.py
+++ core/constants.py
@@ -98,7 +98,7 @@ class Services:
 class Sensors: 
   class Gyro:
     class NAVX2:
-      COM_TYPE = AHRS.NavXComType.kMXP_SPI
+      COM_TYPE = AHRS.NavXComType.kUSB1
   
   class Pose:
     _poseSensorConstants = PoseSensorConstants(
